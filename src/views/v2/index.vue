b180c2c0e725cf27d7c4f1dd6e15ca983a440fcd
jiahe <664215399@qq.com>
2021-06-04 15:01:16 +0800

    fix:v2header修改

--- src/views/v2/index.vue
+++ src/views/v2/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class='Visual'>
+  <div class="Visual">
+    <div class="Visual-header">
+      <h1>可视化面板--V2</h1>
+      </div>
     <div class="Visual-wrapper">
       <visual-left></visual-left>
       <visual-center></visual-center>
@@ -22,13 +25,21 @@ export default {
 
 <style lang='scss' scoped>
 @import '@/assets/scss/common.scss';
-.Visual{
-  background:url('../../assets/images/bgv2.jpg') no-repeat center center/cover;
-  &-wrapper{
-    width:100%;
-    height:100%;
-    background:url('../../assets/images/logov2.png') no-repeat 0 0/contain;
-    padding:100px 40px 0px 40px;
+.Visual {
+  background: url('../../assets/images/bgv2.jpg') no-repeat center center/cover;
+  &-header {
+    line-height: 80px;
+    height: 80px;
+    background: url('../../assets/images/logov2.png') no-repeat center center / cover;
+    h1{
+      text-align: center;
+      font-size: 36px;
+    }
+  }
+  &-wrapper {
+    width: 100%;
+    height: 100%;
+    padding: 0px 40px 0px 40px;
     box-sizing: border-box;
     display: flex;
   }
