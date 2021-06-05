168fc0613847114a99e6bf8ebd5d423b8632e463
jiahe <6624215399@qq.com>
2021-06-05 14:28:51 +0800

    fix:ico不显示

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
       return {
         plugins: [
           new CompressionPlugin({
-            test: /\.js$|\.html$|\.css$|\.jpg$|\.jpeg$|\.png/, // 需要压缩的文件类型
+            test: /\.js$|\.html$|\.css$|\.jpg$|\.jpeg$|\.png$|\.ico/, // 需要压缩的文件类型
             threshold: 10240, // 归档需要进行压缩的文件大小最小值，我这个是10K以上的进行压缩
             deleteOriginalAssets: false, // 是否删除原文件
             minRatio: 0.8
